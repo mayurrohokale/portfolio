51323f3aaaa09a313c92e2639a124a9201f209c5
mayurrohokale <mayurrohokale12345@gmail.com>
2024-03-08 13:14:35 +0530

    created form to send messages

--- src/component/Contact.js
+++ src/component/Contact.js
@@ -6,8 +6,8 @@ import { FaEarthAsia } from "react-icons/fa6";
 const Icon_arr = [
   {
     Icon: FaAddressBook,
-    name:  "Address",
-    description: "Pune, India"
+    name: "Address",
+    description: "Pune, India",
   },
   {
     Icon: FaPhone,
@@ -17,13 +17,13 @@ const Icon_arr = [
 
   {
     Icon: SiTelegram,
-    name:"Email Address",
-    description: "mayurrohokale1041@gmail.com"
+    name: "Email Address",
+    description: "mayurrohokale1041@gmail.com",
   },
   {
     Icon: FaEarthAsia,
     name: "Website",
-    description: "www.mayurohokale.me"
+    description: "www.mayurohokale.me",
   },
 ];
 
@@ -39,21 +39,61 @@ export default function Contact() {
       </div>
 
       <div className="grid xl:grid-cols-4 lg:grid-cols-2 gap-4 px-12 py-12 items-center place-items-center ">
-        
-        {
-          Icon_arr.map((item) => (
-            <Icon {...item} />
-          ))
-        }
-
+        {Icon_arr.map((item) => (
+          <Icon {...item} />
+        ))}
       </div>
 
-      <div className="mt-12">Form fields</div>
+      <div className="flex justify-center mt-12">
+        <form className="w-full max-w-lg">
+          <div className="mb-4">
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="name"
+              type="text"
+              placeholder="Name"
+              required
+            />
+          </div>
+          <div className="mb-4">
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="email"
+              type="email"
+              placeholder="Email Address"
+              required
+            />
+          </div>
+          <div className="mb-4">
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="subject"
+              type="text"
+              placeholder="Subject"
+            />
+          </div>
+          <div className="mb-6">
+            <textarea
+              className="shadow appearance-none border rounded w-full py-8 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="message"
+              placeholder="Message"
+            ></textarea>
+          </div>
+          <div className="flex items-center justify-center">
+            <button
+              className="bg-custom-blue hover:text-black text-white font-semibold py-4 px-7 rounded-[30px] focus:outline-none focus:shadow-outline w-[200px] text-[16px]"
+              type="submit"
+            >
+              SEND MESSAGE
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
 
-function Icon({Icon, name, description}) {
+function Icon({ Icon, name, description }) {
   return (
     <div className="shadow-2xl w-full hover:bg-black/5 items-center flex flex-col justify-center py-5">
       {/* <div className=" flex items-center justify-center h-16 w-16 rounded-full bg-custom-blue text-white shadow-2xl px-2 py-2 outline-2">
@@ -67,5 +107,3 @@ function Icon({Icon, name, description}) {
     </div>
   );
 }
-
-
